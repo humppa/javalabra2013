3f3c33bb88070f82b088b1c2f77054d08028d1ec
rohamo <rohamo@cs.helsinki.fi>
2013-03-26 20:58:34 +0200

    logiikan refaktorointi

--- Ristinolla/src/ristinolla/Logiikka.java
+++ Ristinolla/src/ristinolla/Logiikka.java
@@ -2,20 +2,10 @@ package ristinolla;
 
 public final class Logiikka {
 
-    private int[] pelialusta;
-    private int pelinaloittaja;
-    private int merkkienMaara;
-    private int pelinumero;
-    private int ristinVoitot;
-    private int ympyranVoitot;
-
+    private int[] pelialusta = new int [9];
+    private int pelinaloittaja, pelinumero, ristinVoitot, ympyranVoitot = 0;
+    
     public Logiikka() {
-        this.ristinVoitot = 0;
-        this.ympyranVoitot = 0;
-        this.pelinumero = 0;
-        this.merkkienMaara = 0;
-        this.pelinaloittaja = 0;
-        this.pelialusta = new int[9];
         uusiPeli();
     }
 
@@ -23,55 +13,33 @@ public final class Logiikka {
         for (int i = 0; i < pelialusta.length; i++) {
             this.pelialusta[i] = 0;
         }
-        if (pelinumero == 0) {
-            this.pelinaloittaja = 1;
-        } else {
-            if (this.pelinaloittaja == 1) {
-                this.pelinaloittaja = 2;
-            } else if (this.pelinaloittaja == 2) {
-                this.pelinaloittaja = 1;
-            } else if (this.pelinaloittaja == 0) {
-                this.pelinaloittaja = 1;
-            }
-        }
         pelinumero++;
+        this.pelinaloittaja = getPelinAloittaja();
     }
 
     public int getPelivuorossa() {
-        if (this.getMerkkienMaara() < 0) {
-            return this.pelinaloittaja;
-        } else if (this.getMerkkienMaara() == 1) {
-            if (this.pelinaloittaja == 1) {
-                return 2;
-            } else {
-                return 1;
-            }
-        }
-
-        if (this.pelinaloittaja == 1) {
+        if (getPelinAloittaja() == 1) {
             if (this.getMerkkienMaara() % 2 == 0) {
                 return 1;
             } else {
                 return 2;
-            }
-        } else if (this.pelinaloittaja == 2) {
-            if (this.getMerkkienMaara() % 2 == 0) {
-                return 2;
-            } else {
-                return 1;
             }
         } else {
-            return this.pelinaloittaja;
-        }
+            if (this.getMerkkienMaara() % 2 == 0) {
+                return 2;
+            } else {
+                return 1;
+            }
+        } 
     }
 
-//    public int getPelinAloittaja() {
-//        if (pelinumero > 1 && pelinumero % 2 == 0) {
-//            return 2;
-//        } else {
-//            return 1;
-//        }
-//    }
+    public int getPelinAloittaja() {
+        if (pelinumero() > 1 && pelinumero() % 2 == 0) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
     public int getMerkkienMaara() {
         int merkkeja = 0;
         for (int i = 0; i < this.pelialusta.length; i++) {
@@ -85,11 +53,7 @@ public final class Logiikka {
     public void setMerkkiRuutuun(int ruutu) {
         if (this.pelialusta[ruutu] == 0) {
             if (ruutu >= 0 && ruutu < 9) {
-                if (getPelivuorossa() == 1) {
-                    this.pelialusta[ruutu] = 1;
-                } else {
-                    this.pelialusta[ruutu] = 2;
-                }
+                this.pelialusta[ruutu] = getPelivuorossa();
             } else {
                 throw new IllegalArgumentException("Ruudun tulee olla ruudukon sisällä");
             }
@@ -99,10 +63,10 @@ public final class Logiikka {
     }
 
     public int getRuudunMerkki(int ruutu) {
-        if (this.pelialusta[ruutu] == 0 || ruutu < 9 || ruutu >= 0) {
+        if (this.pelialusta[ruutu] == 0 && ruutu < 9 && ruutu >= 0) {
             return this.pelialusta[ruutu];
         } else {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("Ruutu on pelialustan ulkopuolella");
         }
     }
 
@@ -110,23 +74,33 @@ public final class Logiikka {
         if (this.getMerkkienMaara() < 5) {
             return 0;
         }
-        int x = 0;
+        return tarkistaPystyrivit() + tarkistaVaakarivit() + tarkistaVinorivit();
+    }
+    
+    public int tarkistaPystyrivit() {
         for (int i = 0; i < 3; i++) {
-            //  Tarkistaa pystyrivit
             if (this.pelialusta[i] != 0 && this.pelialusta[i] == this.pelialusta[i + 3] && this.pelialusta[i] == this.pelialusta[i + 6]) {
                 return this.pelialusta[i];
             }
-            //  Tarkistaa vaakarivit
+        }
+        return 0;
+    }
+    
+    public int tarkistaVaakarivit() {
+        int x = 0;
+        for (int i = 0; i < 3; i++) {
             if (this.pelialusta[x] != 0 && this.pelialusta[x] == this.pelialusta[x + 1] && this.pelialusta[x] == this.pelialusta[x + 2]) {
                 return this.pelialusta[x];
             }
             x = x + 3;
         }
-        //  Tarkistaa vinorivin oikeelta ylhaalta vasemmalle alas
+        return 0;
+    }
+    
+    public int tarkistaVinorivit() {
         if (this.pelialusta[0] != 0 && this.pelialusta[0] == this.pelialusta[4] && this.pelialusta[0] == this.pelialusta[8]) {
             return this.pelialusta[0];
         }
-        //  Tarkistaa vinorivin vasemmalta alhaalta oikealle ylos
         if (this.pelialusta[0] != 0 && this.pelialusta[2] == this.pelialusta[4] && this.pelialusta[2] == this.pelialusta[6]) {
             return this.pelialusta[2];
         }
